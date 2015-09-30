b5ad0729c27a8c73be8392ec34425671304722f7
Matt Paul <mattpaul001@icloud.com>
2015-09-30 16:08:32 +0100

    Basic html created, ready for jQuery

--- spec/InterfaceSpec.js
+++ spec/InterfaceSpec.js
@@ -1,16 +1,19 @@
-describe('Interface', function() {
-
-  var thermostat;
-
-  beforeEach(function() {
-    thermostat = new Thermostat();
-    loadFixtures('Thermostat.html');
-    setUpHTMLFixture();
-  });
-
-  it('displays the current temperature', function(){
-    // $('#my-fixture').myTestedPlugin();
-    expect(document).toBeVisible
-  });
-
-});
+// describe('Interface', function() {
+//
+//   var thermostat;
+//
+//   beforeEach(function() {
+//     jasmine.getFixtures().fixturesPath = '.';
+//
+//     // thermostat = new Thermostat();
+//     loadFixtures('Thermostat.html');
+//
+//   });
+//
+//   it('displays the current temperature', function() {
+//
+//     expect($('<div id="display">10</div>')).toHaveText("20")
+//
+//   });
+//
+// });
